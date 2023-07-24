d8dd66179113afc749c83d38284f4bbc880a9ca7
RockRoller01 <joshuaalexanderkahle@gmail.com>
2023-07-24 09:22:37 +0200

    add function to retrieve last 5 skins

--- app/Models/Skin.php
+++ app/Models/Skin.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Forum\Topic;
@@ -9,6 +10,12 @@ use App\Models\Forum\Topic;
 class Skin extends Model
 {
     use SoftDeletes;
+
+    public static function latestSkins($count = 5){
+        return Cache::remember("skins_latest_{$count}", 3600, function () use ($count) {
+            return self::limit($count)->get();
+        });
+    }
 
     public function topic()
     {
